6bb783628671dd9d4411631532c230a6d345e957
niemal <niemal@gmx.com>
2015-10-05 02:24:10 +0300

    Typo fix.

--- src/page_install.php
+++ src/page_install.php
@@ -107,7 +107,7 @@ if (isset($_POST['submit'])) {
 										id INT UNSIGNED NOT NULL,
 										ref TINYTEXT DEFAULT NULL,
 										times INT NOT NULL
-						;)");
+						);");
 				}
 
 				$user =& $parsed_data['admin-user'];
