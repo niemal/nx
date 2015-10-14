b93af2f94310d223cf3112670f92538961bea5c8
niemal <niemal@gmx.com>
2015-10-14 18:55:53 +0300

    UI improvement because inex is too busy to help with this shit

--- src/admin/statistics.php
+++ src/admin/statistics.php
@@ -48,16 +48,23 @@
 					<div id="clear_uri_btn" class="pure-button">Clear</div>
 				</div>
 
-				<div style="margin-top: 30px" class="pure-form">
-					<label for="urls" class="pure-checkbox">
-						<input id="urls" type="checkbox"> URLs
-					</label>
 
-					<label for="refs" class="pure-checkbox">
-						<input id="refs" type="checkbox"> Referers
-					</label>
-					<div id="ref_section" style="margin-top: 10px; margin-left: 30px;">
-						<b>Referers</b>&nbsp;<input required name="ref" readonly="readonly">
+				<div style="margin-top: 30px" class="pure-form">
+					<label for="urls">URLs</label>
+					<img id="urls_tick" src="assets/tick_disabled.png">
+					<img id="urls_x" src="assets/x_enabled.png">
+					<input style="visibility: hidden;" name="urls">
+				</div>
+
+
+				<div class="pure-form">
+					<label for="refs">Referers</label>
+					<img id="refs_tick" src="assets/tick_disabled.png">
+					<img id="refs_x" src="assets/x_enabled.png">
+					<input style="visibility: hidden;" name="refs">
+
+					<div id="ref_section" class="sections">
+						<input required name="ref" readonly="readonly">
 						<select id="ref_opts">
 							<option>ALL</option>
 							<option>this_ref</option>
@@ -67,12 +74,17 @@
 						<div id="del_ref_btn" class="pure-button">Delete</div>
 						<div id="clear_ref_btn" class="pure-button">Clear</div>
 					</div>
+				</div>
 
-					<label for="oss" class="pure-checkbox">
-						<input id="oss" type="checkbox"> Operating systems
-					</label>
-					<div id="os_section" style="margin-top: 10px; margin-left: 30px;">
-						<b>OS</b>&nbsp;<input required name="os" readonly="readonly">
+
+				<div class="pure-form" style="margin-top: 15px">
+					<label for="oss">Operating systems</label>
+					<img id="oss_tick" src="assets/tick_disabled.png">
+					<img id="oss_x" src="assets/x_enabled.png">
+					<input style="visibility: hidden;" name="oss">
+
+					<div id="os_section" class="sections">
+						<input required name="os" readonly="readonly">
 						<select id="os_opts">
 							<option>ALL</option>
 							<option>this_os</option>
@@ -82,13 +94,17 @@
 						<div id="del_os_btn" class="pure-button">Delete</div>
 						<div id="clear_os_btn" class="pure-button">Clear</div>
 					</div>
+				</div>
 
 
-					<label for="browsers" class="pure-checkbox">
-						<input id="browsers" type="checkbox"> Browsers
-					</label>
-					<div id="browser_section" style="margin-top: 10px; margin-left: 30px;">
-						<b>Browsers</b>&nbsp;<input required name="browser" readonly="readonly">
+				<div class="pure-form" style="margin-top: 15px">
+					<label for="browsers">Browsers</label>
+					<img id="browsers_tick" src="assets/tick_disabled.png">
+					<img id="browsers_x" src="assets/x_enabled.png">
+					<input style="visibility: hidden;" name="browsers">
+
+					<div id="browser_section" class="sections">
+						<input required name="browser" readonly="readonly">
 						<select id="browser_opts">
 							<option>ALL</option>
 							<option>this_browser</option>
@@ -98,12 +114,15 @@
 						<div id="del_browser_btn" class="pure-button">Delete</div>
 						<div id="clear_browser_btn" class="pure-button">Clear</div>
 					</div>
-
-					<label for="uas" class="pure-checkbox">
-						<input id="uas" type="checkbox"> User agents
-					</label>
 				</div>
 
+
+				<div class="pure-form" style="margin-top: 15px">
+					<label for="uas">User agents</label>
+					<img id="uas_tick" src="assets/tick_disabled.png">
+					<img id="uas_x" src="assets/x_enabled.png">
+					<input style="visibility: hidden;" name="uas">
+				</div>
 
 
 				<div style="margin-top: 30px" class="pure-form">
@@ -125,19 +144,32 @@
 
 	<script>
 		var tokens = ['uri', 'ref', 'os', 'browser'];
+		var img_tokens = ['urls', 'refs', 'oss', 'browsers', 'uas']
 
-		for (var i=0; i<4; i++)
-			assign_buttons(tokens[i]);
+		tokens.forEach(function(token) { assign_buttons(token); });
+		img_tokens.forEach(function(img_token) { assign_toggles_imgs(img_token); });
 
-		setInterval(check_checkboxes(), 50);
-
-		function check_checkboxes()
+		function assign_toggles_imgs(img_token)
 		{
-			for (var i=0; i<4; i++) {
-				if (document.getElementById(tokens[i] + 's').checked)
-					document.getElementById(tokens[i] + '_section').style.display = 'block';
-				else
-					document.getElementById(tokens[i] + '_section').style.display = 'none';
+			document.getElementById(img_token + '_tick').onclick = function(e) {
+				this.src = 'assets/tick_enabled.png';
+				document.getElementById(img_token + '_x').src = 'assets/x_disabled.png';
+
+				document.getElementsByName(img_token)[0].value = 'true';
+
+				var token = img_token.substring(0, img_token.length-1);
+				if (token !== 'ua' || token !== 'url')
+					document.getElementById(token + '_section').style.display = 'block';
+			}
+
+			document.getElementById(img_token + '_x').onclick = function(e) {
+				this.src = 'assets/x_enabled.png';
+				document.getElementById(img_token + '_tick').src = 'assets/tick_disabled.png';
+
+				document.getElementsByName(img_token)[0].value = '';
+				var token = img_token.substring(0, img_token.length-1);
+				if (token !== 'ua' || token !== 'url')
+					document.getElementById(token + '_section').style.display = 'none';
 			}
 		}
 
@@ -149,9 +181,14 @@
 						elem = document.getElementsByName(token)[0];
 
 					var selected = opts.options[opts.selectedIndex].text;
+
 					if (selected === 'ALL') elem.value = selected;
 					if (elem.value.indexOf('ALL') !== -1) elem.value = '';
-					if (elem.value.indexOf(selected) === -1) elem.value += ' ' + selected;
+
+					if (elem.value.indexOf(selected) === -1) {
+						if (elem.value === '') elem.value = selected;
+						else                   elem.value += ' ' + selected;
+					}
 				}
 			})(token);
 
@@ -161,6 +198,9 @@
 						elem = document.getElementsByName(token)[0];
 						
 					elem.value = elem.value.replace(opts.options[opts.selectedIndex].text, '');
+					var last_val_index = elem.value.length-1;
+					if (elem.value[last_val_index] === ' ')
+						elem.value = elem.value.substring(0, last_val_index);
 				}
 			})(token);
 
